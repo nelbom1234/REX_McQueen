b6057d33f05fbdfe0974b3ab87f0a4749b94d135
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-09-05 15:47:30 +0200

    penis

--- dans.py
+++ dans.py
@@ -9,15 +9,15 @@ print("Running ...")
 
 
 # send a go_diff command to drive forward
-leftSpeed = 100
-rightSpeed = 100
+leftSpeed = 127
+rightSpeed = 127
 i=1
 while(i!=100):
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-    sleep(0.1)
+    sleep(0.5)
     print(arlo.stop())
     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 0))
-    sleep(0.1)
+    sleep(0.5)
     print(arlo.stop())
     i=i+1
 
