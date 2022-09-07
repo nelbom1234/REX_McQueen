defe696cc443696311c6eafdcb8e040ca85deeda
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-09-07 12:19:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -12,20 +12,20 @@ rightSpeed = 66
 print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 sleep(3)
 print(arlo.stop())
-sleep(0.041)
+sleep(0.1)
 
 leftSpeed = 64
 rightSpeed = 64
 print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
 sleep(1.43)
 print(arlo.stop())
-sleep(0.041)
+sleep(0.1)
 
 leftSpeed = 64
 rightSpeed = 66
 print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 sleep(3)
-sleep(0.041)
+sleep(0.1)
 
 print("finished")
 
