3adc96d898bf8d7ab39e17d91a61639f61efc43f
nelbom1234 <nelbom1234@gmail.com>
2022-09-07 11:37:19 +0200

    adding speed_estimation

---
+++ speed_estimation.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+import robot
+
+arlo = robot.Robot()
+
+print("Running ...")
+
+leftSpeed = 64
+rightSpeed = 64
+
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+sleep(3)
+print(arlo.stop())
+sleep(0.041)
+
+print("finished")
