a09544ca302ad79839c6222b419fac8f050d9971
nelbom1234 <nelbom1234@gmail.com>
2022-09-07 14:39:49 +0200

    test

--- test.py
+++ test.py
@@ -9,23 +9,11 @@ print("Running ...")
 
 leftSpeed = 64
 rightSpeed = 66
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+print("driving")
+arlo.go_diff(leftSpeed, rightSpeed, 1, 1)
+print("sleeping")
 sleep(3)
-print(arlo.stop())
-sleep(0.1)
-
-leftSpeed = 64
-rightSpeed = 64
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-sleep(1.435)
-print(arlo.stop())
-sleep(0.1)
-
-leftSpeed = 64
-rightSpeed = 66
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-sleep(3)
-sleep(0.1)
+print("done")
 
 print("finished")
 
