9881abeb6b32a156c87b0cbde4a4ada44d0478a0
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-09-28 10:33:22 +0200

    Add files via upload

---
+++ ffindlandmark.py
@@ -0,0 +1,27 @@
+import string
+from time import sleep
+from turtle import left
+import camera
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+camera1 = camera.Camera(camidx=1, robottype='arlo')
+robottype == 'arlo'
+
+print("Running ...")
+
+
+# send a go_diff command to drive forward
+leftSpeed = 64
+rightSpeed = 66
+
+leftForward = 64
+rightForward = 66
+leftTurn = 64
+rightTurn = 64
+
+print(camera1.detect_aruco_objects(camera1.get_next_frame()))
+
+print("Finished")
