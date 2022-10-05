6688b653639655337ab07683c08620867c7d600d
nelbom1234 <nelbom1234@gmail.com>
2022-10-05 11:03:52 +0200

    dsd

--- ex4/sselflocalize.py
+++ ex4/sselflocalize.py
@@ -220,7 +220,7 @@ try:
                     p.setWeight(p.getWeight() * np.exp(-dists[i]**2/(2*sigma**2)))
                 sum_of_weights += p.getWeight()
             for p in particles:
-                print(p.getWeight() + " " + sum_of_weights)
+                print(p.getWeight().ToString() + " " + sum_of_weights.ToString())
                 p.setWeight(p.getWeight()/sum_of_weights)
                 
 
