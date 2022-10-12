72195489f53d89ff8a00011167aa87cd0f235ee1
nelbom1234 <nelbom1234@gmail.com>
2022-10-12 11:01:55 +0200

    oops

--- ex4/sselflocalize.py
+++ ex4/sselflocalize.py
@@ -288,7 +288,7 @@ try:
                         p.setWeight(p.getWeight() * dist_w * angle_w)
                 sum_of_weights += p.getWeight()
             for p in particles:
-                p.setWeight(p.getWeight()/sum_of_weights)                
+                p.setWeight((p.getWeight()/sum_of_weights))                
 
             # Resampling
             # XXX: You do this
