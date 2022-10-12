83d4df8671595239f23f6933537db5a327d729fe
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-10-12 11:26:05 +0200

    yooo

--- ex4/ssselflocalize.py
+++ ex4/ssselflocalize.py
@@ -205,7 +205,7 @@ try:
         # XXX: You do this
 
         # Do a full turn and update the particles
-        fullTurnVal=3.065
+        fullTurnVal=3
         turnsAmount=6
 
         #SKAL DREJE 360 GRADER
