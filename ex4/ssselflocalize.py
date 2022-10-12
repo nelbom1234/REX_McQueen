4074a3f0a475e97478910e97239f0ee4e61e124d
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-10-12 13:04:42 +0200

    fuck mang

--- ex4/ssselflocalize.py
+++ ex4/ssselflocalize.py
@@ -207,7 +207,7 @@ try:
         # Do a full turn and update the particles
         turnsAmount=12
         speedMultiple=0.75
-        fullTurnVal=2.75/speedMultiple
+        fullTurnVal=2.825/speedMultiple
 
         #SKAL DREJE 360 GRADER
         if fullTurn < turnsAmount:
