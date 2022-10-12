f0e840a7fc059b2bbd3cd998e4633b23ba5e237a
nelbom1234 <nelbom1234@gmail.com>
2022-10-12 11:49:53 +0200

    oops

--- ex4/sselflocalize.py
+++ ex4/sselflocalize.py
@@ -139,7 +139,7 @@ try:
 
 
     # Initialize particles
-    num_particles = 10000
+    num_particles = 3000
     particles = initialize_particles(num_particles)
 
     est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
