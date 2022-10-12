246cfa79b2e74bb83fbc0f07c08459062b2577a6
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-10-12 11:51:22 +0200

    Update movementest.py

--- ex4/movementest.py
+++ ex4/movementest.py
@@ -243,8 +243,8 @@ try:
         hell = 0
         
         for i in range(10):
-            print(arlo.go_diff(64, 64, 0, 1))
-            sleep(0.3)
+            print(arlo.go_diff(32, 32, 0, 1))
+            sleep(0.5)
             print(arlo.stop())
             sleep(0.4)
             hell += 1
@@ -252,13 +252,13 @@ try:
                   if objectIDs[0] == 11 and objectIDs[1] == 10 and hell<4:    
                      print("11")
                      print(arlo.go_diff(32, 32, 1, 0))
-                     sleep(0.3*(hell/2))
+                     sleep(0.5*(hell/2))
                      print(arlo.stop())
                      sleep(0.041)
                   if objectIDs[0] == 10 and objectIDs[1] == 11 and hell<4:
                      print("10")
                      print(arlo.go_diff(32, 32, 1, 0))
-                     sleep(0.3*(hell/2))
+                     sleep(0.5*(hell/2))
                      print(arlo.stop())
                      sleep(0.041)
    
