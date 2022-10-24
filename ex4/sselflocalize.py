aa47a83035c3b2ce68ecfb21b63ad70391aa555e
nelbom1234 <nelbom1234@gmail.com>
2022-10-24 14:24:30 +0200

    help

--- ex4/sselflocalize.py
+++ ex4/sselflocalize.py
@@ -283,7 +283,7 @@ try:
             sigma_dist = 1
             sigma_angle = 1
             sum_of_weights = 0
-            particle.add_uncertainty(particles, 2.5, 0.2)
+            particle.add_uncertainty(particles, 10.0, 0.5)
             for p in particles:
                 for i in range(len(monoObjects)):
                     if monoObjects[i] != None:
