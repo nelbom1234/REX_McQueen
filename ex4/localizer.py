3c332376dd87f51721ca6c1b92667d51f75fc9b5
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-10-26 09:31:12 +0200

    ff

--- ex4/localizer.py
+++ ex4/localizer.py
@@ -256,7 +256,7 @@ try:
                 sleep(3*(dist/124))
                 print(arlo.stop())
                 sleep(0.041)
-                break
+                #break
 
             if not isinstance(objectIDs, type(None)) and all(p == 4 or p == 7 for p in objectIDs):
                 # List detected objects
