0ba8d872c2cd8d0cf04fd8002377bd7c15debf52
nelbom1234 <nelbom1234@gmail.com>
2022-10-30 15:23:23 +0100

    fuck school on sundays

--- ex4/testt.py
+++ ex4/testt.py
@@ -243,22 +243,22 @@ try:
                 if objectIDs[i] == 2:
                     if monoObjects[0] == None:
                         monoObjects[0] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[0][0] > dists[i]*dist_mul:
+                    elif monoObjects[0][0] < dists[i]*dist_mul:
                         monoObjects[0] = (dists[i]*dist_mul, angles[i])
                 elif objectIDs[i] == 1:
                     if monoObjects[1] == None:
                         monoObjects[1] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[1][0] > dists[i]*dist_mul:
+                    elif monoObjects[1][0] < dists[i]*dist_mul:
                         monoObjects[1] = (dists[i]*dist_mul, angles[i])
                 elif objectIDs[i] == 4:
                     if monoObjects[2] == None:
                         monoObjects[2] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[2][0] > dists[i]*dist_mul:
+                    elif monoObjects[2][0] < dists[i]*dist_mul:
                         monoObjects[2] = (dists[i]*dist_mul, angles[i])
                 elif objectIDs[i] == 3:
                     if monoObjects[3] == None:
                         monoObjects[3] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[3][0] > dists[i]*dist_mul:
+                    elif monoObjects[3][0] < dists[i]*dist_mul:
                         monoObjects[3] = (dists[i]*dist_mul, angles[i])
                 
                 
