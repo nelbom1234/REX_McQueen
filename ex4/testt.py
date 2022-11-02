fe2d1e6c3940aa431ad5d181e42c86cb84aeea09
nelbom1234 <nelbom1234@gmail.com>
2022-11-02 14:19:41 +0100

    help

--- ex4/testt.py
+++ ex4/testt.py
@@ -233,13 +233,13 @@ try:
             dvtheta_deg = dvtheta*57.29
             theta_diff = theta-dvtheta
             if theta_diff < 0:
-                print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
-                sleep(0.322*((-theta_diff)/0.333*np.pi))
+                print(arlo.go_diff(leftTurn, rightTurn, 0, 1))
+                sleep(0.322*((-theta_diff)/0.333))
                 print(arlo.stop())
                 sleep(0.041)
             else:
-                print(arlo.go_diff(leftTurn, rightTurn, 0, 1))
-                sleep(0.322*(theta_diff/0.333*np.pi))
+                print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
+                sleep(0.322*(theta_diff/0.333))
                 print(arlo.stop())
                 sleep(0.041)
             dist = np.sqrt(dvx**2+dvy**2)
