dc2353ba6602e947258f48b82239f03844bbf9a8
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-11-02 10:29:12 +0100

    Update iftrying.py

--- ex4/iftrying.py
+++ ex4/iftrying.py
@@ -80,8 +80,8 @@ def draw_world(est_pose, particles, world):
     This functions draws robots position in the world coordinate system."""
 
     # Fix the origin of the coordinate system
-    offsetX = 100 #50
-    offsetY = 250 #50
+    offsetX = 50 #50
+    offsetY = 50 #50
 
     # Constant needed for transforming from world coordinates to screen coordinates (flip the y-axis)
     ymax = world.shape[0]
