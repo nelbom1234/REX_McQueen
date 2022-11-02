5e4968b25d293857e9e57dfec6d7fbe0bcdf4b58
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-11-02 11:58:00 +0100

    Update Rally.py

--- ex4/Rally.py
+++ ex4/Rally.py
@@ -14,7 +14,10 @@ import copy
 showGUI = True  # Whether or not to open GUI windows
 onRobot = True # Whether or not we are running on the Arlo robot
 
-if (onRobot==True):
+def IsRunningOnArlo():
+    Return(onRobot)
+    
+if IsRunningOnArlo():
     # XXX: You need to change this path to point to where your robot.py file is located
     sys.path.append("/home/pi/Arlo/Robot/git/REX_McQueen")
 
