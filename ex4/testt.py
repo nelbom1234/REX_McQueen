d56594d9b4329e42fa5ec2936ff1fdeb77d72326
nelbom1234 <nelbom1234@gmail.com>
2022-11-02 11:32:07 +0100

    help

--- ex4/testt.py
+++ ex4/testt.py
@@ -142,7 +142,7 @@ try:
 
 
     # Initialize particles
-    num_particles = 1200
+    num_particles = 2000
     particles = initialize_particles(num_particles)
 
     est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
