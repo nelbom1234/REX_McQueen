33e1f41ba09bf03e424f28c0a3f819baf057286c
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-11-02 13:47:11 +0100

    Create test.py

--- Arkiv/test.py
+++ Arkiv/test.py
@@ -9,11 +9,42 @@ print("Running ...")
 
 leftSpeed = 64
 rightSpeed = 66
-print("driving")
-arlo.go_diff(leftSpeed, rightSpeed, 1, 1)
-print("sleeping")
-sleep(3)
-print("done")
+arlo.go_diff(leftSpeed, rightSpeed, 1, 0)
+timer(3)
+
+
+hello = True
+
+def timer(x):
+    start = time.time()
+    end = start + (x*1000)
+    while hello==True:
+        if time.time() == end:
+            hello = False
+        elif arlo.read_front_ping_sensor() < 50:
+            hello = False
+        else:
+            Print("going")
+
+
+  # request to read Front sonar ping sensor
+print("Front sensor = ", arlo.read_front_ping_sensor())
+sleep(0.041)
+
+
+  # request to read Back sonar ping sensor
+print("Back sensor = ", arlo.read_back_ping_sensor())
+sleep(0.041)
+
+  # request to read Right sonar ping sensor
+print("Right sensor = ", arlo.read_right_ping_sensor())
+sleep(0.041)
+
+# request to read Left sonar ping sensor
+print("Left sensor = ", arlo.read_left_ping_sensor())
+sleep(0.041)
+
+
 
 print("finished")
 
