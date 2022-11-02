ceff40c882d8e789f9055b40b1cc71588e19bd17
nelbom1234 <nelbom1234@gmail.com>
2022-11-02 15:09:07 +0100

    help

--- ex4/testt.py
+++ ex4/testt.py
@@ -237,12 +237,12 @@ try:
             
             if theta_diff < 0:
                 print(arlo.go_diff(leftTurn, rightTurn, 0, 1))
-                sleep(2.500*((-theta_diff)/(2.0*np.pi)))
+                sleep(3.000*((-theta_diff)/(2.0*np.pi)))
                 print(arlo.stop())
                 sleep(0.041)
             else:
                 print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
-                sleep(2.500*(theta_diff/(2.0*np.pi)))
+                sleep(3.000*(theta_diff/(2.0*np.pi)))
                 print(arlo.stop())
                 sleep(0.041)
             dist = np.sqrt(dvx**2+dvy**2)
