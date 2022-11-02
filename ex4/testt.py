e18aa0bc8387041653c0687dc0878fd48d0e780b
nelbom1234 <nelbom1234@gmail.com>
2022-11-02 11:16:07 +0100

    help

--- ex4/testt.py
+++ ex4/testt.py
@@ -174,7 +174,7 @@ try:
     fullTurnAmount = 0
     turns = 0
     Skip=0
-    dist_mul = 1
+    dist_add = 20
 
     while True:
 
@@ -238,28 +238,28 @@ try:
         if not isinstance(objectIDs, type(None)) and any(p < 5 for p in objectIDs):
             # List detected objects
             for i in range(len(objectIDs)):
-                print("Object ID = ", objectIDs[i], ", Distance = ", dists[i]*dist_mul, ", angle = ", angles[i])
+                print("Object ID = ", objectIDs[i], ", Distance = ", dists[i]+dist_mul, ", angle = ", angles[i])
                 # XXX: Do something for each detected object - remember, the same ID may appear several times
                 if objectIDs[i] == 1:
                     if monoObjects[0] == None:
-                        monoObjects[0] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[0][0] < dists[i]*dist_mul:
-                        monoObjects[0] = (dists[i]*dist_mul, angles[i])
+                        monoObjects[0] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[0][0] < dists[i]+dist_mul:
+                        monoObjects[0] = (dists[i]+dist_mul, angles[i])
                 elif objectIDs[i] == 2:
                     if monoObjects[1] == None:
-                        monoObjects[1] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[1][0] < dists[i]*dist_mul:
-                        monoObjects[1] = (dists[i]*dist_mul, angles[i])
+                        monoObjects[1] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[1][0] < dists[i]+dist_mul:
+                        monoObjects[1] = (dists[i]+dist_mul, angles[i])
                 elif objectIDs[i] == 3:
                     if monoObjects[2] == None:
-                        monoObjects[2] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[2][0] < dists[i]*dist_mul:
-                        monoObjects[2] = (dists[i]*dist_mul, angles[i])
+                        monoObjects[2] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[2][0] < dists[i]+dist_mul:
+                        monoObjects[2] = (dists[i]+dist_mul, angles[i])
                 elif objectIDs[i] == 4:
                     if monoObjects[3] == None:
-                        monoObjects[3] = (dists[i]*dist_mul, angles[i])
-                    elif monoObjects[3][0] < dists[i]*dist_mul:
-                        monoObjects[3] = (dists[i]*dist_mul, angles[i])
+                        monoObjects[3] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[3][0] < dists[i]+dist_mul:
+                        monoObjects[3] = (dists[i]+dist_mul, angles[i])
                 
                 
 
