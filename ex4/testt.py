4921d3e3e45c5e49fd345b88608221601aef977d
nelbom1234 <nelbom1234@gmail.com>
2022-11-04 13:33:22 +0100

    help

--- ex4/testt.py
+++ ex4/testt.py
@@ -259,7 +259,7 @@ try:
 
             if theta_diff < 0.0:
                 if dvx < 0:
-                    turns = turns - 6.0
+                    turns = turns - 5.0
                 while -turns > 0.0:
                     if -turns > 1:
                         print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 0, 1))
@@ -275,7 +275,7 @@ try:
                         turns = 0
             else:
                 if dvx < 0:
-                    turns = turns + 6.0
+                    turns = turns + 5.0
                 while turns > 0.0:
                     if turns > 1.0:
                         print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 1, 0))
