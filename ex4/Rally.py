e6bbc0fa5d6c24633dd8e083ed4a04e83b6dea42
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-11-04 12:04:53 +0100

    gregerg

--- ex4/Rally.py
+++ ex4/Rally.py
@@ -29,81 +29,7 @@ except ImportError:
     print("selflocalize.py: robot module not present - forcing not running on Arlo!")
     onRobot = False
 
-# Some color constants in BGR format
-CRED = (0, 0, 255)
-CGREEN = (0, 255, 0)
-CBLUE = (255, 0, 0)
-CCYAN = (255, 255, 0)
-CYELLOW = (0, 255, 255)
-CMAGENTA = (255, 0, 255)
-CWHITE = (255, 255, 255)
-CBLACK = (0, 0, 0)
 
-# Landmarks.
-# The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 2, 3, 4]
-landmarks = {
-    1: (0.0, 0.0),  # Coordinates for landmark 1
-    2: (0.0, 300.0),  # Coordinates for landmark 2
-    3: (400.0, 0.0),  # Coordinates for landmark 3
-    4: (400.0, 300.0) # Coordinates for landmark 4
-}
-landmark_colors = [CRED, CGREEN, CBLUE, CYELLOW] # Colors used when drawing the landmarks
-
-
-def jet(x):
-    """Colour map for drawing particles. This function determines the colour of 
-    a particle from its weight."""
-    r = (x >= 3.0/8.0 and x < 5.0/8.0) * (4.0 * x - 3.0/2.0) + (x >= 5.0/8.0 and x < 7.0/8.0) + (x >= 7.0/8.0) * (-4.0 * x + 9.0/2.0)
-    g = (x >= 1.0/8.0 and x < 3.0/8.0) * (4.0 * x - 1.0/2.0) + (x >= 3.0/8.0 and x < 5.0/8.0) + (x >= 5.0/8.0 and x < 7.0/8.0) * (-4.0 * x + 7.0/2.0)
-    b = (x < 1.0/8.0) * (4.0 * x + 1.0/2.0) + (x >= 1.0/8.0 and x < 3.0/8.0) + (x >= 3.0/8.0 and x < 5.0/8.0) * (-4.0 * x + 5.0/2.0)
-
-    return (255.0*r, 255.0*g, 255.0*b)
-
-def draw_world(est_pose, particles, world):
-    """Visualization.
-    This functions draws robots position in the world coordinate system."""
-
-    # Fix the origin of the coordinate system
-    offsetX = 50
-    offsetY = 50
-
-    # Constant needed for transforming from world coordinates to screen coordinates (flip the y-axis)
-    ymax = world.shape[0]
-    
-    #do 
-    #do
-    #do
-    world[:] = CBLACK # Clear background to white 
-    # do
-    
-    # Find largest weight
-    max_weight = 0
-    for particle in particles:
-        max_weight = max(max_weight, particle.getWeight())
-
-    # Draw particles
-    for particle in particles:
-        x = int(particle.getX() + offsetX)
-        y = ymax - (int(particle.getY() + offsetY))
-        colour = jet(particle.getWeight() / max_weight)
-        cv2.circle(world, (x,y), 2, colour, 2)
-        b = (int(particle.getX() + 15.0*np.cos(particle.getTheta()))+offsetX, 
-                                     ymax - (int(particle.getY() + 15.0*np.sin(particle.getTheta()))+offsetY))
-        cv2.line(world, (x,y), b, colour, 2)
-
-    # Draw landmarks
-    for i in range(len(landmarkIDs)):
-        ID = landmarkIDs[i]
-        lm = (int(landmarks[ID][0] + offsetX), int(ymax - (landmarks[ID][1] + offsetY)))
-        cv2.circle(world, lm, 5, landmark_colors[i], 2)
-
-    # Draw estimated robot pose
-    a = (int(est_pose.getX())+offsetX, ymax-(int(est_pose.getY())+offsetY))
-    b = (int(est_pose.getX() + 15.0*np.cos(est_pose.getTheta()))+offsetX, 
-                                 ymax-(int(est_pose.getY() + 15.0*np.sin(est_pose.getTheta()))+offsetY))
-    cv2.circle(world, a, 5, CMAGENTA, 2)
-    cv2.line(world, a, b, CMAGENTA, 2)
 
 
 
@@ -151,7 +77,7 @@ try:
     world = np.zeros((400,500,3), dtype=np.uint8)
 
     # Draw map
-    draw_world(est_pose, particles, world)
+    AuxFunctions.draw_world(est_pose, particles, world)
 
     print("Opening and initializing camera")
     if isRunningOnArlo():
@@ -159,8 +85,8 @@ try:
     else:
         cam = camera.Camera(0, 'macbookpro', useCaptureThread = True)
 
-    est_pose, particles=AuxFunctions.LocalizeRobot(particles=particles, num_particles=num_particles,landmarks=landmarks,cam=cam,arlo=arlo,world=world)
-    draw_world(est_pose, particles, world)
+    est_pose, particles=AuxFunctions.LocalizeRobot(particles=particles, num_particles=num_particles,cam=cam,arlo=arlo,world=world)
+    AuxFunctions.draw_world(est_pose, particles, world)
     sleep(20)
     
     
