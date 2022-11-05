b126dd90d45fb177c829a28e9fb62e90a8932736
nelbom1234 <nelbom1234@gmail.com>
2022-11-05 14:23:02 +0100

    help

--- ex4/Rally.py
+++ ex4/Rally.py
@@ -85,13 +85,13 @@ def drive_to_coordinates(x_end, y_end, est_pose):
             turns = turns - 4.0
         while -turns > 0.0:
             if -turns > 1:
-                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 0, 1))
+                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 1, 0))
                 sleep(0.322)
                 print(arlo.stop())
                 sleep(0.1)
                 turns = turns + 1.0
             else:
-                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 0, 1))
+                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 1, 0))
                 sleep(0.322*(-turns))
                 print(arlo.stop())
                 sleep(0.1)
@@ -101,13 +101,13 @@ def drive_to_coordinates(x_end, y_end, est_pose):
             turns = turns + 4.0
         while turns > 0.0:
             if turns > 1.0:
-                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 1, 0))
+                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 0, 1))
                 sleep(0.322)
                 print(arlo.stop())
                 sleep(0.1)
                 turns = turns - 1.0
             else:
-                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 1, 0))
+                print(arlo.go_diff(leftTurn*speedMultiple, rightTurn*speedMultiple, 0, 1))
                 sleep(0.322*turns)
                 print(arlo.stop())
                 sleep(0.1)
