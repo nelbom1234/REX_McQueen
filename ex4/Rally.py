927f3da6cd9efd682abfd29b71a36fdbdcc0f2e6
nelbom1234 <nelbom1234@gmail.com>
2022-11-05 13:25:39 +0100

    help

--- ex4/Rally.py
+++ ex4/Rally.py
@@ -165,7 +165,7 @@ try:
     else:
         cam = camera.Camera(0, 'macbookpro', useCaptureThread = True)
     
-    ListOfCoordinates = [[30, 30], [30, 250], [370, 30], [370, 250], [30, 30]]
+    ListOfCoordinates = [[30, 30], [30, 270], [370, 30], [370, 270], [30, 30]]
 
     DrivingPlan(ListOfCoordinates)
 
