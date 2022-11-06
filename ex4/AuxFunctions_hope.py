b47ba99c0662cb00d328dfadf7165e3bcc835288
nelbom1234 <nelbom1234@gmail.com>
2022-11-06 11:01:28 +0100

    help

--- ex4/AuxFunctions_hope.py
+++ ex4/AuxFunctions_hope.py
@@ -143,29 +143,29 @@ def LocalizeRobot(num_particles,cam,arlo,world,goal):
             #if any(p == goal or (p == 1 and goal == 5) for p in objectIDs):
             #    return None
             #else:
-                for i in range(len(objectIDs)):
-                    print("Object ID = ", objectIDs[i], ", Distance = ", dists[i]+dist_mul, ", angle = ", angles[i])
-                    # XXX: Do something for each detected object - remember, the same ID may appear several times
-                    if objectIDs[i] == 1:
-                        if monoObjects[0] == None:
-                            monoObjects[0] = (dists[i]+dist_mul, angles[i])
-                        elif monoObjects[0][0] < dists[i]+dist_mul:
-                            monoObjects[0] = (dists[i]+dist_mul, angles[i])
-                    elif objectIDs[i] == 2:
-                        if monoObjects[1] == None:
-                            monoObjects[1] = (dists[i]+dist_mul, angles[i])
-                        elif monoObjects[1][0] < dists[i]+dist_mul:
-                            monoObjects[1] = (dists[i]+dist_mul, angles[i])
-                    elif objectIDs[i] == 3:
-                        if monoObjects[2] == None:
-                            monoObjects[2] = (dists[i]+dist_mul, angles[i])
-                        elif monoObjects[2][0] < dists[i]+dist_mul:
-                            monoObjects[2] = (dists[i]+dist_mul, angles[i])
-                    elif objectIDs[i] == 4:
-                        if monoObjects[3] == None:
-                            monoObjects[3] = (dists[i]+dist_mul, angles[i])
-                        elif monoObjects[3][0] < dists[i]+dist_mul:
-                            monoObjects[3] = (dists[i]+dist_mul, angles[i])
+            for i in range(len(objectIDs)):
+                print("Object ID = ", objectIDs[i], ", Distance = ", dists[i]+dist_mul, ", angle = ", angles[i])
+                # XXX: Do something for each detected object - remember, the same ID may appear several times
+                if objectIDs[i] == 1:
+                    if monoObjects[0] == None:
+                        monoObjects[0] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[0][0] < dists[i]+dist_mul:
+                        monoObjects[0] = (dists[i]+dist_mul, angles[i])
+                elif objectIDs[i] == 2:
+                    if monoObjects[1] == None:
+                        monoObjects[1] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[1][0] < dists[i]+dist_mul:
+                        monoObjects[1] = (dists[i]+dist_mul, angles[i])
+                elif objectIDs[i] == 3:
+                    if monoObjects[2] == None:
+                        monoObjects[2] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[2][0] < dists[i]+dist_mul:
+                        monoObjects[2] = (dists[i]+dist_mul, angles[i])
+                elif objectIDs[i] == 4:
+                    if monoObjects[3] == None:
+                        monoObjects[3] = (dists[i]+dist_mul, angles[i])
+                    elif monoObjects[3][0] < dists[i]+dist_mul:
+                        monoObjects[3] = (dists[i]+dist_mul, angles[i])
                 
                 
 
