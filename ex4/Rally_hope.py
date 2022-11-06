62dcace3a9e7bd2862510ef183b5f06f4ab6c4ff
dennienuridov <112876158+dennienuridov@users.noreply.github.com>
2022-11-06 14:05:23 +0100

    Update Rally_hope.py

--- ex4/Rally_hope.py
+++ ex4/Rally_hope.py
@@ -82,6 +82,11 @@ def Avoid_to_the_Front():
     print(arlo.go_diff(leftForward, rightForward, 1, 1))
     timer(1.5)
     print(arlo.stop())
+    
+    sleep(0.041)
+    print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
+    sleep(0.637)
+    print(arlo.stop())
 
 def Avoid_to_the_Right():
     print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
@@ -109,6 +114,11 @@ def Avoid_to_the_Right():
 
     print(arlo.go_diff(leftForward, rightForward, 1, 1))
     timer(0.5)
+    print(arlo.stop())
+    
+    sleep(0.041)
+    print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
+    sleep(0.637)
     print(arlo.stop())
 
 def Avoid_to_the_left():
@@ -139,6 +149,11 @@ def Avoid_to_the_left():
 
     print(arlo.go_diff(leftForward, rightForward, 1, 1))
     timer(0.5)
+    print(arlo.stop())
+    
+    sleep(0.041)
+    print(arlo.go_diff(leftTurn, rightTurn, 0, 1))
+    sleep(0.637)
     print(arlo.stop())
  
 
