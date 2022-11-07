f4db8c9d3b6a5ed4e1ddacc7988362cb01fc8ed6
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-11-07 11:53:35 +0100

    iuhiuh

--- ex4/AuxFunctions_hope.py
+++ ex4/AuxFunctions_hope.py
@@ -270,15 +270,6 @@ def Avoid_to_the_Front(arlo):
     sleep(0.637)
     print(arlo.stop())
 
-    print(arlo.go_diff(leftForward, rightForward, 1, 1))
-    timer(2.5,arlo)
-    print(arlo.stop())
-    
-    sleep(0.041)
-    print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
-    sleep(0.637)
-    print(arlo.stop())
-
 def Avoid_to_the_Right(arlo):
     print(arlo.go_diff(leftForward, rightForward, 0, 0))
     sleep(1.0)
@@ -304,15 +295,6 @@ def Avoid_to_the_Right(arlo):
 
     sleep(0.041)
     print(arlo.go_diff(leftTurn, rightTurn, 0, 1))
-    sleep(0.637)
-    print(arlo.stop())
-
-    print(arlo.go_diff(leftForward, rightForward, 1, 1))
-    timer(0.5,arlo)
-    print(arlo.stop())
-    
-    sleep(0.041)
-    print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
     sleep(0.637)
     print(arlo.stop())
 
@@ -346,16 +328,6 @@ def Avoid_to_the_left(arlo):
     print(arlo.go_diff(leftTurn, rightTurn, 1, 0))
     sleep(0.637)
     print(arlo.stop())
-
-    print(arlo.go_diff(leftForward, rightForward, 1, 1))
-    timer(0.5,arlo)
-    print(arlo.stop())
-    
-    sleep(0.041)
-    print(arlo.go_diff(leftTurn, rightTurn, 0, 1))
-    sleep(0.637)
-    print(arlo.stop())
- 
 
 def timer(x,arlo):
     start = time.time()
